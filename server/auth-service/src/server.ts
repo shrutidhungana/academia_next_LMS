420b771e1ed93be8c241c71ae45ceec9b0515ae3
shrutidhungana <shrutidhungana123@gmail.com>
2025-07-11 17:07:06 +0545

    first commit

---
+++ server/auth-service/src/server.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import "./database";
+
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_BASE_URL,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+  res.send("Auth Service Running");
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`🚀 Auth Service listening on port ${PORT}`);
+});
